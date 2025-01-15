90fb14493d5abcd6528e87776106acd6a4c8841b
Tech_Messiah <123579120+TechMessiah@users.noreply.github.com>
2025-01-15 12:24:49 +0300

    Modified the interface

--- main.py
+++ main.py
@@ -14,11 +14,45 @@ def lookUp(word):
 
     return result
 
+# function used by button to lookup words
+def search():
+    word = entry.get()
+    if len(word) > 0 :
+        result = lookUp(word)
+        print(result)
 
 root = ttk.Window() # same as tk.Tk() from tkinter
-style = ttk.Style("flatly") # setting theme for the interface
-style.configure("TButton", font=("Arial", 12, "bold"), padding=10) # setting style for buttons
+
+style = ttk.Style("litera") # setting theme for the interface
+style.configure("Outline.TButton", font=("Arial", 12, "bold"), padding=10, relief=RAISED) # setting style for buttons
+style.configure('TEntry', font=('Helvetica', 18), padding=10, relief=RAISED) # setting style for entry field
+
 root.title("Dictionary")
 root.geometry("600x400")
 root.resizable(False, False)
+
+# Banner
+banner = ttk.Frame(root)
+banner.pack(pady=20)
+
+# Logo
+logo = tk.PhotoImage(file="images/logo.png")
+logoLabel = ttk.Label(root, image=logo)
+logoLabel.pack(pady=10)
+
+# Welcome message
+welcome = ttk.Label(root, text="Welcome !", font=("Arial", 30, "bold"))
+welcome.pack(pady=10)
+
+# Search Box Frame
+searchBox = ttk.Frame(root)
+searchBox.pack(pady=20)
+
+# Entry field
+entry = ttk.Entry(searchBox, width=50, font=("Arial", 12))
+entry.pack(side=LEFT, padx=5)
+
+# Button
+button = ttk.Button(searchBox, text="Look Up", command=search, style='Outline.TButton')
+button.pack(side=RIGHT, padx=5)
 root.mainloop()
