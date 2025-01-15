cbfa701751c08abd280872b1d5bbd16a7b2e9d55
TechMessiah <mr.jeromeogwang@gmail.com>
2025-01-15 17:23:32 +0300

    Added ScrollArea

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
+from ttkbootstrap.scrolled import ScrolledFrame
 import requests
 
 def lookUp(word):
@@ -15,13 +16,21 @@ def lookUp(word):
     return result
 
 # function used by button to lookup words
-def search():
+def search(resultsArea):
     word = entry.get()
     if len(word) > 0 :
         result = lookUp(word)
+        
         print(result)
+            
+        definitions = ttk.Label(resultsArea, text=result, wraplength=600) 
+        definitions.pack(pady=10)
 
 root = ttk.Window() # same as tk.Tk() from tkinter
+
+# enabling scrolling
+mainLayout = ScrolledFrame(root, autohide=True)
+mainLayout.pack(fill=BOTH, expand=YES, padx=10, pady=10)
 
 style = ttk.Style("litera") # setting theme for the interface
 style.configure("Outline.TButton", font=("Arial", 12, "bold"), padding=10, relief=RAISED) # setting style for buttons
@@ -36,15 +45,15 @@ banner.pack(pady=20)
 
 # Logo
 logo = tk.PhotoImage(file="images/logo.png")
-logoLabel = ttk.Label(root, image=logo)
+logoLabel = ttk.Label(mainLayout, image=logo)
 logoLabel.pack(pady=10)
 
 # Welcome message
-welcome = ttk.Label(root, text="Welcome !", font=("Arial", 30, "bold"))
+welcome = ttk.Label(mainLayout, text="Welcome !", font=("Arial", 30, "bold"))
 welcome.pack(pady=10)
 
 # Search Box Frame
-searchBox = ttk.Frame(root)
+searchBox = ttk.Frame(mainLayout)
 searchBox.pack(pady=20)
 
 # Entry field
@@ -52,6 +61,7 @@ entry = ttk.Entry(searchBox, width=50, font=("Arial", 12))
 entry.pack(side=LEFT, padx=5)
 
 # Button
-button = ttk.Button(searchBox, text="Look Up", command=search, style='Outline.TButton')
+button = ttk.Button(searchBox, text="Look Up", command=lambda: search(mainLayout), style='Outline.TButton')
 button.pack(side=RIGHT, padx=5)
+
 root.mainloop()
